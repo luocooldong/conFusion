d31af308101967eadcc051dabe41c3ad9dfd917f
Yudong Geng <gengyudonga@gmail.com>
2017-07-25 22:42:56 +0800

    Single Page Applications Part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Dish } from '../shared/dish';
 
+
+import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 
@@ -15,19 +16,10 @@ export class MenuComponent implements OnInit {
 
   dishes: Dish[];
 
-  selectedDish: Dish;
+  constructor(private dishservice: DishService) { }
 
-  constructor(private dishService: DishService) { 
-
-  }
-  //生命周期方法，在Component初始化和创建的时候都会执行
   ngOnInit() {
-    this.dishes = this.dishService.getDishes();
-  }
-
-  onSelect(dish: Dish) {
-    console.log(dish);
-    this.selectedDish = dish;
+    this.dishes = this.dishservice.getDishes();
   }
 
 }
